7105a363b059959f31af5b5bf954d85f2d8cc5d2
George Tudor <Alexander.George.Tudor@gmail.com>
2022-03-12 23:47:58 -0600

    first commit

---
+++ src/main/java/com/georgetudor/tshirt/TshirtApplication.java
@@ -0,0 +1,13 @@
+package com.georgetudor.tshirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TshirtApplication.class, args);
+    }
+
+}
